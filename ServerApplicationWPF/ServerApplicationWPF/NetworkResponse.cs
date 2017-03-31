de9c757ebd3758454ff4325b4e5cfebfb16c5014
Martino Mensio <martinomensio@outlook.it>
2017-03-31 02:35:26 +0200

    network classes implemented

--- ServerApplicationWPF/ServerApplicationWPF/NetworkResponse.cs
+++ ServerApplicationWPF/ServerApplicationWPF/NetworkResponse.cs
@@ -6,14 +6,22 @@ using System.Threading.Tasks;
 
 namespace ServerApplicationWPF
 {
-    class NetworkResponse
+    public class NetworkResponse
     {
         public byte[] Payload { get; private set; }
         public ResponseType responseType { get; private set; }
         public enum ResponseType
         {
             ImageProcessingResult = 2,
-            ReceiptStorageResult = 3
+            ImageProcessingError = 3,
+            ReceiptStorageResult = 4,
+            ReceiptStorageError = 5
+        }
+
+        public NetworkResponse(ResponseType type, byte[] payload)
+        {
+            responseType = type;
+            Payload = payload;
         }
     }
 }
