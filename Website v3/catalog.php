71d1a65a88efffbd03fd08fc0cddf27d390eb954
sorath.asnani <sorath.asnani@hotmail.com>
2017-06-09 10:13:57 +0200

    Website v3 has been uploaded

---
+++ Website v3/catalog.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+include 'dbconnect.php';
+include 'time_elapse.php';
+
+// if HTTPS is off, redirect to same page with HTTPS enabled
+if (! isset ( $_SERVER ['HTTPS'] ) || $_SERVER ['HTTPS'] != "on") {
+	header ( "Location: https://" . $_SERVER ['HTTP_HOST'] . $_SERVER ['REQUEST_URI'] );
+	exit ();
+}
+
+//if user is not logged in, redirect to index.php
+if (! isset ( $_SESSION ['userId'] )) {
+	session_destroy ();
+	header ( "Location: index.php" );
+	exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- Redirect to test_javaScript.php if javaScript is not enabled -->
+<!--<noscript> <meta http-equiv="refresh" content="0;url=test_javascript.php"> </noscript>-->
+
+<meta charset="ISO-8859-1">
+<title>Home</title>
+
+<!-------------------------- BootStrap Files ---------------------->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="js/jquery.js"></script>
+<script src="js/footer.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-------------------------- BootStrap Files ---------------------->
+
+</head>
+<body class="times-new-roman">
+	<div class="container-fluid">
+		<div class="row rm bg-primary">
+			<div class="col-md-8">
+				<h1><a class="header" href="index.php">electronics.com</a></h1>
+			</div>
+		<div class="col-md-4">
+			<span class="login-text pull-right"> 
+			<?php if (isset ( $_SESSION ['userId'] ) != "") { ?>
+					Logged in as <?php echo $_SESSION ['username'] . " | "; ?>
+					<a class="login-text" href='logout.php'>Logout</a>
+			<?php } ?>
+			</span>
+		</div>
+		</div>
+		<div class="row rm">
+			<br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+			<div class="col-md-4 text-center">
+				<br /> <br /> <br /> <br />
+				<a class="btn btn-success btn-text" href="view_customers.php">View Customers</a>
+				<br /> <br /> <br /> <br />
+			</div>
+			<div class="col-md-4 text-center">
+				<br /> <br /> <br /> <br />
+				<a class="btn btn-success btn-text" href="my_profile.php">Manage Products</a>
+				<br /> <br /> <br /> <br />
+			</div>
+			<div class="col-md-4 text-center">
+				<br /> <br /> <br /> <br />
+				<a class="btn btn-success btn-text" href="view_purchases.php">View Purchases</a>
+				<br /> <br /> <br /> <br />
+			</div>
+			
+		</div>
+		<div class="row rm">
+			<div class="footer bg-primary" id="footer">
+				<br />
+				<p>Copyrights @ All rights are reserved by MSG Team, 2017</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
